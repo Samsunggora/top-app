40e691026658ce8593effe73948b34913677b3a0
Samsunggora <samsunggora@gmail.com>
2022-09-17 21:32:43 +0200

    add new component: Rating
    and start write lout

---
+++ layout/Layout.tsx
@@ -0,0 +1,13 @@
+import styles from "./Layout.module.css";
+import {LayoutProps} from "./Layout.props";
+
+const Layout = ({children}: LayoutProps): JSX.Element => {
+    return (
+        <>
+
+        </>
+    );
+
+};
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,43 @@
+import Htag from "../components/Htag/Htag";
+import Button from "../components/Button/Button";
+import P from "../components/P/P";
+import Tag from "../components/Tag/Tag";
+import {useEffect, useState} from "react";
+import Rating from "../components/Rating/Rating";
+
+export default function Home(): JSX.Element {
+    const [counter, setCounter] = useState<number>(0);
+    useEffect(() => {
+        console.log(counter);
+        return function cleanUp() {
+            console.log("Unmount" + counter);
+        }
+    });
+    return (
+        <div>
+            <Htag tag='h1'>{counter}</Htag>
+            <Button appearance="ghost" onClick={() => setCounter(x => x + 1)}> Button counter + </Button>
+            <Button appearance="primary" arrow="right">Button</Button>
+            <Button appearance="primary" arrow="down">Button</Button>
+            <Button appearance="ghost" arrow="right">Button</Button>
+            <Button appearance="ghost" arrow="down">Button</Button>
+            <P size='s'>Hello word!</P>
+            <P size='m'>Hello word!</P>
+            <P size='l'>Hello word!</P>
+            <P>Hello word!</P>
+            <Tag color="ghost">Hello teg!</Tag>
+            <Tag color="red">Hello teg!</Tag>
+            <Tag color="grey">Hello teg!</Tag>
+            <Tag color="green">Hello teg!</Tag>
+            <Tag color="primary">Hello teg!</Tag>
+            <Tag size="m">Hello teg!</Tag>
+            <Tag size="s">Hello teg!</Tag>
+            <Tag href="http://localhost:3000/">Hello teg!</Tag>
+            <Rating rating={1} isEditable/>
+            <Rating rating={2}/>
+            <Rating rating={3}/>
+            <Rating rating={4}/>
+            <Rating rating={5}/>
+        </div>
+    );
+}
